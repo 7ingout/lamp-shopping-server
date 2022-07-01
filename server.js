ad469f7fa46de80a3dd8ede996a9e813880a2aa7
7ingout <hadvjin@naver.com>
2022-07-01 14:42:58 +0900

    상품조회완료서버

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const models = require('./models');
 
 // json형식의 데이터를 처리할 수 있게 설정
 app.use(express.json());
@@ -11,68 +12,44 @@ app.use(cors());
 // 요청처리
 // app.메서드(url, 함수)
 app.get('/products',async(req,res)=>{
-    const result = {
-        products: [
-            {
-                id: 1,
-                name: "거실조명",
-                price: 50000,
-                imgsrc:"images/products/product1.jpg",
-                seller: "green",
-            },
-            {
-                id: 2,
-                name: "아동조명",
-                price: 50000,
-                imgsrc:"images/products/product2.jpg",
-                seller: "green",
-            },
-            {
-                id: 3,
-                name: "부엌조명",
-                price: 50000,
-                imgsrc:"images/products/product3.jpg",
-                seller: "green",
-            },
-            {
-                id: 4,
-                name: "베란다조명",
-                price: 50000,
-                imgsrc:"images/products/product4.jpg",
-                seller: "green",
-            },
-            {
-                id: 5,
-                name: "아무조명",
-                price: 50000,
-                imgsrc:"images/products/product1.jpg",
-                seller: "green",
-            },
-            {
-                id: 6,
-                name: "몰라조명",
-                price: 50000,
-                imgsrc:"images/products/product2.jpg",
-                seller: "green",
-            },
-            {
-                id: 7,
-                name: "룰루조명",
-                price: 50000,
-                imgsrc:"images/products/product3.jpg",
-                seller: "green",
-            },
-            {
-                id: 8,
-                name: "히히조명",
-                price: 50000,
-                imgsrc:"images/products/product4.jpg",
-                seller: "green",
-            }
-        ]
-    }
-    res.send(result);
+    // 데이터베이스 조회하기
+    models.Product.findAll()
+    .then(result=> {
+        // console.log("제품전체조회", result);
+        res.send(result);
+    })
+    .catch(e=>{
+        console.error(e)
+        res.send("파일 조회에 문제가 있습니다.")
+    })
 })
+// method는 get이고 오고 url은 /product/2 로 요청이 온 경우
+app.get('/product/:id', async (req, res) => {
+    const params = req.params;
+    // const { id } = params;
+    // 하나만 조회할때는 findDone -> select문
+    models.Product.findOne({
+        // 조건절
+        where: {
+            id: params.id
+        }
+    })
+    .then(result=>{
+        res.send(result);
+    })
+    .catch(e=>{
+        console.log(e)
+        res.send("상품 조회에 문제가 생겼습니다.")
+    })
+    // const product = {
+    //     id: id,
+    //     name: "서버에서 보내는 이름",
+    //     price: 50000,
+    //     imgsrc:"images/products/product4.jpg",
+    //     seller: "green",
+    // }
+    // res.send(product);
+});
 app.post('/green', async (req, res)=>{
     console.log(req);
     res.send('그린 게시판에 게시글이 등록되었습니다.');
@@ -80,4 +57,17 @@ app.post('/green', async (req, res)=>{
 // 실행
 app.listen(port, ()=>{
     console.log('쇼핑몰 서버가 동작중입니다.');
+    // sequelize와 데이터베이스 연결작업
+    // 데이터베이스 동기화
+    models.sequelize
+    .sync()
+    .then(()=> {
+        console.log('DB연결 성공');
+    })
+    .catch(e=>{
+        console.error(e);
+        console.log('DB연결 에러');
+        // 서버실행이 안되면 프로세서를 종료
+        process.exit();
+    })
 })
